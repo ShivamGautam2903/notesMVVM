0fe6fdb30d95fe804fd502ba814897f6cdfec7c4
ShivamGautam <gautam.shivam@paytm.com>
2022-08-30 15:04:40 +0530

    v1.2 Added searchview

--- app/src/main/java/com/example/notesmvvm/MainActivity.kt
+++ app/src/main/java/com/example/notesmvvm/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.notesmvvm
+
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.appcompat.widget.SearchView
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.notesmvvm.Database.Note
@@ -14,13 +16,14 @@ import com.example.notesmvvm.RecyclerView.NoteDeleteInterface
 import com.example.notesmvvm.RecyclerView.NoteRVAdapter
 import com.example.notesmvvm.repository.NoteRepository
 import com.example.notesmvvm.databinding.ActivityMainBinding
+import java.util.*
 
-class MainActivity : AppCompatActivity(), NoteClickInterface, NoteDeleteInterface{
+class MainActivity : AppCompatActivity(), NoteClickInterface, NoteDeleteInterface {
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var mainViewModel: MainViewModel
     private lateinit var noteList: List<Note>
-
+    private lateinit var tempNotes: MutableList<Note>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,7 +36,6 @@ class MainActivity : AppCompatActivity(), NoteClickInterface, NoteDeleteInterfac
         val dao = NoteDatabase.getDatabase(applicationContext).noteDao()
         val repository = NoteRepository(dao)
 
-
         mainViewModel =
             ViewModelProvider(this, MainViewModelFactory(repository))[MainViewModel::class.java]
 
@@ -41,6 +43,7 @@ class MainActivity : AppCompatActivity(), NoteClickInterface, NoteDeleteInterfac
         mainViewModel.getNotes().observe(this) {
             adapter.updateList(it)
             noteList = it
+            tempNotes = noteList.toMutableList()
         }
 
         recyclerView.layoutManager = LinearLayoutManager(this)
@@ -51,6 +54,37 @@ class MainActivity : AppCompatActivity(), NoteClickInterface, NoteDeleteInterfac
             Intent(this, CreateNoteActivity::class.java).also {
                 startActivity(it)
             }
+        }
+
+        /* The next block implements SearchView functioning. We create a tempNote mutable List and whenever we make a query
+        the answer is saved in it. At each step we update the adapter list with the new temp list.
+        When we're done*/
+        binding.apply {
+            searchview.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+                override fun onQueryTextSubmit(query: String?): Boolean {
+                    searchview.clearFocus()
+                    return false
+                }
+
+                override fun onQueryTextChange(newText: String?): Boolean {
+                    tempNotes.clear()
+                    val searchText = newText!!.lowercase(Locale.getDefault())
+                    if (searchText.isNotEmpty()) {
+                        noteList.forEach {
+                            if (it.title.lowercase(Locale.getDefault()).contains(searchText)) {
+                                tempNotes.add(it)
+                            }
+                        }
+                        adapter.updateList(tempNotes)
+                        adapter.notifyDataSetChanged()
+                    } else {
+                        tempNotes.clear()
+                        adapter.updateList(noteList)
+                        adapter.notifyDataSetChanged()
+                    }
+                    return false
+                }
+            })
         }
     }
 
